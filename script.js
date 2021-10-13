ed9b36faca1abe3f729e85266b4403c15246e26a
Leonardo Prevost <prevostleonardo@gmail.com>
2021-10-13 17:07:14 -0300

    Added JS in analog clock

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ function updateClock() {
     let minute = now.getMinutes();
     let second = now.getSeconds();
 
-    digitalElement.innerHTML = `${hour}:${minute}:${second}`;
+    digitalElement.innerHTML = `${formatHour(hour)}:${formatHour(minute)}:${formatHour(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 }
+
+function formatHour(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
 
 setInterval(updateClock, 1000);
